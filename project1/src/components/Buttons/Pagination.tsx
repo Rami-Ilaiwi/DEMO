b85982d84a163855fd4f2f56a0e01ded4d7bc3b5
Rami Ilaiwi <rami.ilaiwi1997@gmail.com>
2019-10-10 12:16:01 +0300

    make pagination component

--- project1/src/components/Buttons/Pagination.tsx
+++ project1/src/components/Buttons/Pagination.tsx
@@ -1,104 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 import TablePagination from "@material-ui/core/TablePagination";
 
-export const styles = () =>
-  createStyles({
-    root: {
-      border: "1px solid #ddd",
-      position: "relative",
-      padding: "0.5rem 0.75rem",
-      minWidth: "20px",
-      minHeight: "20px"
-    },
-
-    primary: {
-      color: "#5CB85C",
-      backgroundColor: "#fff",
-      textDecoration: "none"
-    },
-    secondary: {
-      color: "#fff",
-      backgroundColor: "#5CB85C",
-      textDecoration: "underline"
-    }
-  });
-
 interface PaginationProps {
-  numberOfPages: number;
+  articlesCount: number;
+  page: number;
+  onChangePage: (page: number) => void;
 }
 
-const Pagination: React.FC<
-  PaginationProps & WithStyles<typeof styles>
-> = props => {
-  // class Pagination extends React.Component<
-  //   PaginationProps & WithStyles<typeof styles>
-  // > {
-  const [value, setValue] = useState(1);
-  const [numberOfPages, setNumberOfPages] = useState(0);
-  const [page, setPage] = useState(props.numberOfPages);
-
-  useEffect(() => {
-    setNumberOfPages(props.numberOfPages);
-  }, []);
-  const handleChange = (
+const Pagination: React.FC<PaginationProps> = props => {
+  const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
     page: number
   ) => {
-    // this.setState({ value: event.currentTarget.id });
+    props.onChangePage(page);
   };
 
-  const classes = props.classes;
   return (
     <>
-      {console.log(numberOfPages)}
       <TablePagination
-        page={0}
+        page={props.page}
         component="nav"
         rowsPerPage={10}
-        count={500}
-        onChangePage={handleChange}
+        count={props.articlesCount}
+        onChangePage={handleChangePage}
         rowsPerPageOptions={[10]}
       />
-
-      {/* <button
-        className={`${classes.root} ${
-          isActive(1) ? classes.secondary : classes.primary
-        }`}
-        onClick={handleChange}
-        id="1"
-      >
-        1
-      </button>
-      <button
-        className={`${classes.root} ${
-          isActive(2) ? classes.secondary : classes.primary
-        }`}
-        onClick={handleChange}
-        id="2"
-      >
-        2
-      </button>
-      <button
-        className={`${classes.root} ${
-          isActive(3) ? classes.secondary : classes.primary
-        }`}
-        onClick={handleChange}
-        id="3"
-      >
-        3
-      </button>
-      <button
-        className={`${classes.root} ${
-          isActive(4) ? classes.secondary : classes.primary
-        }`}
-        onClick={handleChange}
-        id="4"
-      >
-        4
-      </button> */}
     </>
   );
 };
 
-export default withStyles(styles)(Pagination);
+export default Pagination;
